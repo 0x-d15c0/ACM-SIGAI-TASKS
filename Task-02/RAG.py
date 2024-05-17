d431ad8159e3014f2e1448969aab45969afd215f
Tarun <nottarun7@gmail.com>
2024-05-18 01:47:53 +0530

    added chat history

---
+++ Task-02/RAG.py
@@ -0,0 +1,50 @@
+from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import HuggingFaceBgeEmbeddings
+from langchain_community.llms import Ollama
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains import RetrievalQA
+from langchain.prompts import PromptTemplate
+
+def rag(question):
+
+    model = Ollama(model="phi3")
+
+    huggingface_embeddings = HuggingFaceBgeEmbeddings(
+        model_name="BAAI/bge-small-en-v1.5",
+        model_kwargs={'device': 'cpu'},
+        encode_kwargs={'normalize_embeddings': True}
+    )
+
+    loader = PyPDFDirectoryLoader("./pdf")
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    final_documents = text_splitter.split_documents(documents)
+
+    prompt_template = """
+    You are an intelligent assistant with access to a large set of documents. Answer the following question based on the information from these documents.
+
+    Context: {context}
+
+    Question: {question}
+
+    Answer:
+    """
+
+    vectorstore = FAISS.from_documents(final_documents[:200], huggingface_embeddings)
+
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
+    retrievalQA = RetrievalQA.from_chain_type(
+        llm=model,
+        chain_type="stuff",
+        retriever=retriever,
+        return_source_documents=True,
+        chain_type_kwargs={"prompt": prompt}
+    )
+
+    result = retrievalQA.invoke({"query": question})
+    return result['result']
---
+++ Task-02/frontend.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from RAG import rag
+
+# Function to clear chat history
+def clear_chat_history():
+    st.session_state.messages = []
+
+
+# Main title of the app
+st.title("RAG Chat Bot 🤖")
+
+# Initialize chat history if not already present
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    # Generate assistant response
+    response = rag(prompt)
+    
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+
+# Sidebar to display chat history
+st.sidebar.title("Chat History")
+for i, message in enumerate(st.session_state.messages):
+    if message["role"] == "user":
+        st.sidebar.markdown(f"{message['content']}")
+
+# Set up the sidebar
+st.sidebar.button("Clear Chat History", on_click=clear_chat_history)
